6f2956bcc5beef373931d16961bfe0850cc2349a
Dirceu Belém <dirceu@fourtime.com>
2019-10-08 21:14:36 -0300

    Teste

---
+++ src/main/java/mockup/ServiceFeed.java
@@ -0,0 +1,37 @@
+package mockup;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("feed")
+public class ServiceFeed extends ServiceBase {
+
+    @GET
+    public String feed() {
+        JSONArray ja = new JSONArray();
+
+        JSONObject j;
+
+        JSONArray f = new JSONArray();
+        f.put("Cláudio");
+        f.put("Gustavo");
+        f.put("Carlos");
+        f.put("Carolina");
+        f.put("Henrique");
+
+        j = new JSONObject();
+        j.put("id", Guid.getString());
+        j.put("date", DateTime.now().toString("dd/MM/yyyy HH:mm"));
+        j.put("description", "Imagem 1");
+        j.put("image", "https://mockup.fluo.site/images/1.png");
+        j.put("likes", f);
+
+        ja.put(j);
+
+        return ja.toString();
+    }
+
+}
