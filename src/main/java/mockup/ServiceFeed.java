b2d485658a4cfef0cda62ba687568d9a01757328
Dirceu Belém <dirceu@fourtime.com>
2019-10-08 21:34:11 -0300

    Subindo feed

--- src/main/java/mockup/ServiceFeed.java
+++ src/main/java/mockup/ServiceFeed.java
@@ -27,11 +27,175 @@ public class ServiceFeed extends ServiceBase {
         j = new JSONObject();
         j.put("id", Guid.getString());
         j.put("date", DateTime.now().toString("dd/MM/yyyy HH:mm"));
-        j.put("description", "Imagem 1");
+        j.put("description", "Dia de Chuva");
         j.put("image", "https://mockup.fluo.site/images/1.png");
         j.put("likes", f);
 
         ja.put(j);
+
+
+        f = new JSONArray();
+        f.put("Maria");
+        f.put("Gustavo");
+        f.put("Carlos");
+        f.put("Carolina");
+        f.put("Henrique");
+        f.put("Dirceu");
+        f.put("Roberto");
+        f.put("Vitor");
+        f.put("Alessandro");
+
+        j = new JSONObject();
+        j.put("id", Guid.getString());
+        j.put("date", DateTime.now().toString("dd/MM/yyyy HH:mm"));
+        j.put("description", "Really enjoyed the movie");
+        j.put("image", "https://mockup.fluo.site/images/2.png");
+        j.put("likes", f);
+
+        ja.put(j);
+
+
+        f = new JSONArray();
+        f.put("Maria");
+        f.put("Gustavo");
+        f.put("Dirceu");
+        f.put("Roberto");
+        f.put("Vitor");
+        f.put("Alessandro");
+
+        j = new JSONObject();
+        j.put("id", Guid.getString());
+        j.put("date", DateTime.now().toString("dd/MM/yyyy HH:mm"));
+        j.put("description", "Entardecer");
+        j.put("image", "https://mockup.fluo.site/images/3.png");
+        j.put("likes", f);
+
+        ja.put(j);
+
+
+        f = new JSONArray();
+        f.put("Maria");
+        f.put("Gustavo");
+        f.put("Vitor");
+        f.put("Alessandro");
+
+        j = new JSONObject();
+        j.put("id", Guid.getString());
+        j.put("date", DateTime.now().toString("dd/MM/yyyy HH:mm"));
+        j.put("description", "Photo by @awwnuhh42");
+        j.put("image", "https://mockup.fluo.site/images/4.png");
+        j.put("likes", f);
+
+        ja.put(j);
+
+
+        f = new JSONArray();
+        f.put("Maria");
+        f.put("Gustavo");
+        f.put("Carlos");
+        f.put("Carolina");
+        f.put("Henrique");
+        f.put("Dirceu");
+        f.put("Roberto");
+        f.put("Vitor");
+        f.put("Alessandro");
+        f.put("Geraldo");
+        f.put("Renato");
+        f.put("Cristina");
+
+        j = new JSONObject();
+        j.put("id", Guid.getString());
+        j.put("date", DateTime.now().toString("dd/MM/yyyy HH:mm"));
+        j.put("description", "Sapiens Parque");
+        j.put("image", "https://mockup.fluo.site/images/5.png");
+        j.put("likes", f);
+
+        ja.put(j);
+
+        f = new JSONArray();
+        f.put("Maria");
+        f.put("Gustavo");
+        f.put("Roberto");
+        f.put("Vitor");
+        f.put("Alessandro");
+        f.put("Geraldo");
+        f.put("Renato");
+        f.put("Cristina");
+        
+
+        j = new JSONObject();
+        j.put("id", Guid.getString());
+        j.put("date", DateTime.now().toString("dd/MM/yyyy HH:mm"));
+        j.put("description", "Setup");
+        j.put("image", "https://mockup.fluo.site/images/6.png");
+        j.put("likes", f);
+
+        ja.put(j);
+
+
+        f = new JSONArray();
+        f.put("Maria");
+        f.put("Roberto");
+        f.put("Vitor");
+        f.put("Alessandro");
+        f.put("Geraldo");
+        f.put("Maria");
+        f.put("Gustavo");
+        f.put("Carlos");
+        f.put("Carolina");
+        f.put("Henrique");
+        f.put("Dirceu");
+        f.put("Roberto");
+        f.put("Vitor");
+        f.put("Alessandro");
+        f.put("Geraldo");
+        f.put("Renato");
+        f.put("Cristina");
+        f.put("Renato");
+        f.put("Cristina");
+
+        j = new JSONObject();
+        j.put("id", Guid.getString());
+        j.put("date", DateTime.now().toString("dd/MM/yyyy HH:mm"));
+        j.put("description", "O Cine Theatro Brasil, atualmente chamado Cine Theatro Brasil Vallourec, é um prédio histórico de Belo Horizonte");
+        j.put("image", "https://mockup.fluo.site/images/7.png");
+        j.put("likes", f);
+
+        ja.put(j);
+
+
+
+
+        f = new JSONArray();
+        f.put("Maria");
+        f.put("Roberto");
+        f.put("Vitor");
+        f.put("Alessandro");
+        f.put("Geraldo");
+        f.put("Renato");
+        f.put("Cristina");
+        f.put("Maria");
+        f.put("Gustavo");
+        f.put("Carlos");
+        f.put("Carolina");
+        f.put("Henrique");
+        f.put("Dirceu");
+        f.put("Roberto");
+        f.put("Vitor");
+        f.put("Alessandro");
+        f.put("Geraldo");
+        f.put("Renato");
+        f.put("Cristina");
+
+        j = new JSONObject();
+        j.put("id", Guid.getString());
+        j.put("date", DateTime.now().toString("dd/MM/yyyy HH:mm"));
+        j.put("description", "Alguém sabe onde a foto foi tirada");
+        j.put("image", "https://mockup.fluo.site/images/8.png");
+        j.put("likes", f);
+
+        ja.put(j);
+
 
         return ja.toString();
     }
